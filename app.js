9d245f71e7ec73b89735a2001890ffb8c44db73b
w2u2u <warunyu.pon@gmail.com>
2017-12-04 02:42:48 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Work!'))
+
+const PORT = process.env.PORT || 5555
+app.listen(POR, () => console.log(`App listening on ${PORT}...`))
