7724b91c1ca8c066dbf68a7b8209ce41a17c7836
w2u2u <warunyu.pon@gmail.com>
2017-12-05 11:21:01 +0700

    Creating the dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const router = express.Router()
+
+const Story = mongoose.model('story')
 
 const { ensureAuthenticated, ensureGuest } = require('../helper/auth')
 
 router.get('/', ensureGuest, (req, res) => res.render('index/welcome'))
 
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
-  res.render('index/dashboard')
-)
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+  Story.find({ owner: req.user.id }).then(stories => {
+    res.render('index/dashboard', { stories: stories })
+  })
+})
 
 router.get('/about', (req, res) => res.render('index/about'))
 
